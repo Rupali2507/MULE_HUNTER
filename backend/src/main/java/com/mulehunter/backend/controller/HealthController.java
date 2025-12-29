d7ccdceb7cef607d3b9070d6302cceb8d2971aee
Prisha Garg <prishag.ug23.ma@nitp.ac.in>
2025-12-29 13:17:16 +0530

    fixed health controller

--- backend/src/main/java/com/mulehunter/backend/controller/HealthController.java
+++ backend/src/main/java/com/mulehunter/backend/controller/HealthController.java
@@ -1,3 +1,16 @@
+package com.mulehunter.backend.controller;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
